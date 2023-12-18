c9ea96daecec29456f51f44ef9903ebf21e2d53d
AleKs <alexanderfranciscog@gmail.com>
2023-12-17 23:29:06 -0400

    config entity manager

---
+++ src/main/java/com/th3alexdev/petcarejava/utils/PersistenceManager.java
@@ -0,0 +1,15 @@
+package com.th3alexdev.petcarejava.utils;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class PersistenceManager {
+    private static final EntityManagerFactory entityManagerFactory = entityManagerFactoryProvider();
+    private static EntityManagerFactory entityManagerFactoryProvider () {
+        return Persistence.createEntityManagerFactory("petcarejpa");
+    }
+    public static EntityManager getEntityManager() {
+        return entityManagerFactory.createEntityManager();
+    }
+}
